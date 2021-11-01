e7f8e0920148abbbe0fadc512c87e780bbebf5c1
radeonxt <alexnm1@hotmail.com>
2021-11-01 07:33:17 -0300

    commit inicial

---
+++ src/main/java/com/project/mythingscollection/MyThingsCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.project.mythingscollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyThingsCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyThingsCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mythingscollection/MyThingsCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mythingscollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThingsCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
